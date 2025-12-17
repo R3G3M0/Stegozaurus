9a968f40cd0242e926559d925c8155e0f9fc659e
AlexsTS <alex.tsaregorodtsev.br@gmail.com>
2025-12-17 19:16:24 +0300

    Loaded old project

---
+++ CryptoGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Steganography
+{
+    public class CryptoGenerator
+    {
+        public List<int> generate(string password, int max)
+        {
+            //int[] sequence;
+            int hash = makeHash(password);
+
+            Random rand = new Random(hash);
+            List<int> randInt = new List<int>();
+            //int max = bitmap.PixelHeight * bitmap.PixelWidth - 1;
+
+            for (int i = 0, k = 0; i < (max / 2); i++)
+            {
+                bool equ = false;
+                k = rand.Next(max);
+                foreach (int ar in randInt)
+                    if (k == ar) equ = true;
+                if (!equ)
+                    randInt.Add(k);
+            }
+
+            return randInt;
+        }
+        private int makeHash(string password)
+        {
+            int p = 0;
+            int i = 1;
+            int p1 = 0;
+  
+            foreach (char element in password)
+            {
+                p += i * (int)element;
+                i++;
+            }
+
+            p1 = p % 1327;
+            if (p1 == 0)
+                p1 = p % 1361;
+
+
+            return p1;
+        }
+    }
+}
---
+++ Preprocessor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Steganography
+{
+    
+    class Preprocessor
+    {
+        private int messageLength;
+        private int imageSize;
+        public int GetLength(int width, int height)
+        {
+            const double embedingPersent = 0.1; //процент встраиваемой информации от размеров изображения
+            int maxMesLength = 0;
+            int pixelCount = width * height;
+            int bitsInsertedInPix = 2; // для 1 метода 2 бита информации в пиксель
+            // для 2 метода 6 бит в пиксель
+
+            //максимальная длина сообщения (в битах)
+            maxMesLength = (int)(pixelCount * embedingPersent * bitsInsertedInPix);
+
+            // logger.WriteLog();
+
+            int maxMesLengthInSymb = maxMesLength / 6;
+
+            // logger.InformUser();
+
+            return maxMesLengthInSymb; 
+        }
+
+        public bool CheckLang(string mes, int flag)
+        {
+            string pattern;
+            
+            // по умолчанию 0 - англ язык
+            if(flag == 0)
+            {
+                pattern = @"^[a-zA-Z0-9]*";
+            }
+            else
+            {
+                pattern = @"^[а-яА-Я0-9]*";
+            }
+            Regex regex = new Regex(pattern);
+
+            if(!regex.IsMatch(mes))
+            {
+                
+            }
+
+            return true;
+        }
+
+        public void displayResults()
+        {
+
+        }
+    }
+}
