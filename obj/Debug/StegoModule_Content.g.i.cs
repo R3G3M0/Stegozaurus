0c35beee93d4efacb83b9bd335893191b786ac59
AlexsTS <alex.tsaregorodtsev.br@gmail.com>
2026-02-01 16:04:27 +0300

    Some experements

--- obj/Debug/StegoModule_Content.g.i.cs
+++ obj/Debug/StegoModule_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("stego.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("stegozaurus.ico")]
 
 
